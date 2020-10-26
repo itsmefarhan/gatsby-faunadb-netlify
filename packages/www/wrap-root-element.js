500f389d6d7429c7fc0224761ef9e0af6f70574b
Farhan Farooq <farhan.farooq@live.com>
2020-10-27 03:01:24 +0500

    fauna

--- packages/www/wrap-root-element.js
+++ packages/www/wrap-root-element.js
@@ -7,13 +7,28 @@ const {
   InMemoryCache,
 } = require("@apollo/client");
 const fetch = require("isomorphic-fetch");
+const { setContext } = require("apollo-link-context");
+const netlifyIdentitfy = require("netlify-identity-widget");
+
+const authLink = setContext((_, { headers }) => {
+  const user = netlifyIdentitfy.currentUser();
+  const token = user.token.access_token;
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const httpLink = new HttpLink({
+  uri: "https://gatsby-faunadb.netlify.app/.netlify/functions/graphql",
+  fetch,
+});
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: "https://gatsby-faunadb.netlify.app/.netlify/functions/graphql",
-    fetch,
-  }),
+  link: authLink.concat(httpLink),
 });
 module.exports = ({ element }) => (
   <AuthProvider>
